f5e89ceae9f9bbabfb6ffb10aaf2e3446619818c
rakib2067 <rakib7@hotmail.co.uk>
2021-11-07 01:03:58 +0000

    added validate hooks and validation to basic form

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,22 +1,94 @@
+import { useState } from "react";
+import useValidate from "../hooks/use-validate";
+
 const BasicForm = (props) => {
+  const {
+    value: name,
+    isValid: nameIsValid,
+    hasError: nameHasError,
+    valueBlurHandler: nameBlurHandler,
+    valueChangeHandler: nameChangeHandler,
+    reset: nameReset,
+  } = useValidate((value) => value.trim !== "");
+  const {
+    value: lName,
+    isValid: lNameIsValid,
+    hasError: lNameHasError,
+    valueBlurHandler: lNameBlurHandler,
+    valueChangeHandler: lNameChangeHandler,
+    reset: lNameReset,
+  } = useValidate((value) => value.trim !== "");
+  const {
+    value: email,
+    isValid: emailIsValid,
+    hasError: emailHasError,
+    valueBlurHandler: emailBlurHandler,
+    valueChangeHandler: emailChangeHandler,
+    reset: emailReset,
+  } = useValidate((value) => value.includes("@"));
+
+  let formIsValid = false;
+  const inputIsValid = nameIsValid && emailIsValid && lNameIsValid;
+  if (inputIsValid) {
+    formIsValid = true;
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    nameReset();
+    lNameReset();
+    emailReset();
+  };
+
+  const nameFormClasses = nameHasError
+    ? "form-control invalid"
+    : "form-control";
+  const lNameFormClasses = lNameHasError
+    ? "form-control invalid"
+    : "form-control";
+  const emailFormClasses = emailHasError
+    ? "form-control invalid"
+    : "form-control";
   return (
-    <form>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+    <form onSubmit={handleSubmit}>
+      <div className="control-group">
+        <div className={nameFormClasses}>
+          <label htmlFor="name">First Name</label>
+          <input
+            value={name}
+            onChange={nameChangeHandler}
+            onBlur={nameBlurHandler}
+            type="text"
+            id="name"
+          />
+          {nameHasError && <p className="error-text">Invalid Name</p>}
         </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+        <div className={lNameFormClasses}>
+          <label htmlFor="name">Last Name</label>
+          <input
+            value={lName}
+            onBlur={lNameBlurHandler}
+            onChange={lNameChangeHandler}
+            type="text"
+            id="name"
+          />
+          {lNameHasError && <p className="error-text">Invalid Last Name</p>}
         </div>
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+      <div className={emailFormClasses}>
+        <label htmlFor="name">E-Mail Address</label>
+        <input
+          value={email}
+          onChange={emailChangeHandler}
+          onBlur={emailBlurHandler}
+          type="text"
+          id="name"
+        />
+        {emailHasError && <p className="error-text">Invalid E-Mail</p>}
       </div>
-      <div className='form-actions'>
-        <button>Submit</button>
+      <div className="form-actions">
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
---
+++ src/hooks/use-validate.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+const useValidate = (validateValue) => {
+  const [value, setValue] = useState("");
+  const [valueTouched, setValueTouched] = useState(false);
+
+  const valueIsValid = validateValue(value);
+  const hasError = !valueIsValid && valueTouched;
+
+  const valueChangeHandler = (event) => {
+    setValue(event.target.value);
+  };
+  const valueBlurHandler = (event) => {
+    setValueTouched(true);
+  };
+  const reset = () => {
+    setValue("");
+    setValueTouched(false);
+  };
+
+  return {
+    value,
+    isValid: valueIsValid,
+    hasError,
+    valueChangeHandler,
+    valueBlurHandler,
+    reset,
+  };
+};
+
+export default useValidate;
