49e6bceab5820c14155d91cd983416c04d2588e4
rakib2067 <rakib7@hotmail.co.uk>
2021-11-07 01:26:54 +0000

    Added a reducer to the input context, for simpleForm

--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -1,24 +1,37 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
 
+const inputReducer = (state, action) => {
+  if (action.type === "INPUT") {
+    return { enteredValue: action.value, isTouched: state.isTouched };
+  }
+  if (action.type === "BLUR") {
+    return { enteredValue: state.enteredValue, isTouched: true };
+  }
+  if (action.type === "RESET") {
+    return { enteredValue: "", isTouched: false };
+  }
+  return { enteredValue: "", isTouched: false };
+};
 const useInput = (validateValue) => {
-  const [enteredValue, setEnteredValue] = useState("");
-  const [isTouched, setIsTouched] = useState(false);
+  const [inputState, dispatchInput] = useReducer(inputReducer, {
+    enteredValue: "",
+    isTouched: false,
+  });
 
-  const valueIsValid = validateValue(enteredValue);
-  const hasError = !valueIsValid && isTouched;
+  const valueIsValid = validateValue(inputState.enteredValue);
+  const hasError = !valueIsValid && inputState.isTouched;
 
   const valueChangeHandler = (event) => {
-    setEnteredValue(event.target.value);
+    dispatchInput({ type: "INPUT", value: event.target.value });
   };
   const inputBlurHandler = (event) => {
-    setIsTouched(true);
+    dispatchInput({ type: "BLUR" });
   };
   const reset = () => {
-    setEnteredValue("");
-    setIsTouched(false);
+    dispatchInput({ type: "RESET" });
   };
   return {
-    value: enteredValue,
+    value: inputState.enteredValue,
     isValid: valueIsValid,
     hasError,
     valueChangeHandler,
